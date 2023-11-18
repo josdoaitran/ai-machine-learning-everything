aa64677bae0bfe35c108dfe154a4c8cb4182c5ea
Doai Tran <josdoaitran@gmail.com>
2023-11-18 08:03:41 +0700

    suggestion methods code whisperer

--- example-codewhisperer/codewwhisperer-python/basic_python/start.py
+++ example-codewhisperer/codewwhisperer-python/basic_python/start.py
@@ -5,13 +5,17 @@ import string
 def random_number():
     return random.randint(100, 999)
 
-# generate the list of fake name with minimum length 5 and maximum length 10
-def generate_fake_name(length):
-    return "".join(random.choices(string.ascii_letters, k=length))
+# generate the list of fake name with minimum length is 5 and maximum length is 10
+def fake_name_generator(min_length=5, max_length=10):
+    return ''.join(random.choices(string.ascii_letters, k=random.randint(min_length, max_length)))
     
 
-# write the main method to run script
+# write the executable main method to run this script
+if __name__ == '__main__':
+    print(random_number())
+    print(fake_name_generator())
+    print(fake_name_generator(max_length=8))
+    
 
-if __name__ == "__main__":
-    print(random_number)
-    print(generate_fake_name(7))
+
+
